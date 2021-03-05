a30aa48a07bda3d79ad835b02b34e9bea5369fb4
Liu Ying <cdlliuy@cn.ibm.com>
2021-03-05 12:14:44 +0800

    add the initial demo code

--- cmd/main.go
+++ cmd/main.go
@@ -4,28 +4,30 @@ import (
 	"fmt"
 	"os"
 
-	"knative-sandbox/kn-plugin-diag/pkg/command/diagnose"
-
 	"github.com/spf13/cobra"
+	"github.com/knative-sandbox/kn-plugin-diag/pkg/command/diagnose"
+	"github.com/knative-sandbox/kn-plugin-diag/pkg/utils"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
+
 )
+
 
 func main() {
 
-	//p := &utils.ConnectionConfig{}
-	//p.Initialize()
+	p := &utils.ConnectionConfig{}
+	p.Initialize()
 
 	rootCmd := &cobra.Command{
 		Use:   "kn-diag",
 		Short: "A CLI of to help with Diagnose Knative Resources",
 		Long:  `A CLI of to help with Diagnose Knative Resources.`,
 	}
-	rootCmd.AddCommand(diagnose.NewServiceCmd())
+	rootCmd.AddCommand(diagnose.NewServiceCmd(p))
 	rootCmd.InitDefaultHelpCmd()
-
+	
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println("Failed : ", err)
 		os.Exit(1)
 	}
-
+	
 }
